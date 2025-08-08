5e94ce072e4272d334cba1b01dc303e66fecfd75
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-08-08 14:23:57 +0200

    Initial commit – project setup

---
+++ src/main/java/pl/nicolehoppy/cryptoanalyzer/CryptoAnalyzerApplication.java
@@ -0,0 +1,17 @@
+package pl.nicolehoppy.cryptoanalyzer;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalyzerApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(CryptoAnalyzerApplication.class, args);
+	}
+
+}
